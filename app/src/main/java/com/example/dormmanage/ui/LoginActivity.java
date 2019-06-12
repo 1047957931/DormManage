a11dcae8b1fcfaaf9b48b1e0fda16ebc8dae24cb
Accesslly <541124324@qq.com>
2019-06-12 23:08:50 +0800

    宿舍

--- app/src/main/java/com/example/dormmanage/ui/LoginActivity.java
+++ app/src/main/java/com/example/dormmanage/ui/LoginActivity.java
@@ -11,30 +11,37 @@ import com.example.dormmanage.R;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     private Button signup;
-    private Button login;
+    private Button studentlogin;
+    private Button managerlogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        login=findViewById(R.id.btn_login);
+        studentlogin=findViewById(R.id.btn_student_login);
         signup=findViewById(R.id.btn_signup);
+        managerlogin=findViewById(R.id.btn_manager_login);
 
-        login.setOnClickListener(this);
+        studentlogin.setOnClickListener(this);
         signup.setOnClickListener(this);
+        managerlogin.setOnClickListener(this);
 
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
-            case R.id.btn_login:
+            case R.id.btn_student_login:
                 Intent intent=new Intent(LoginActivity.this,MainActivity.class);
                 startActivity(intent);
                 break;
             case R.id.btn_signup:
                 intent=new Intent(LoginActivity.this,SignUpActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btn_manager_login:
+                intent=new Intent(LoginActivity.this,ManagerActivity.class);
                 startActivity(intent);
                 break;
         }
